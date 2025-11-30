acc35fefbcc7319ce05025633fa1de08c04fa0cd
Le Phan Tri Duc <87744086+lephantriduc@users.noreply.github.com>
2025-11-30 21:21:41 +0700

    fix: Resolve the problem creating too many connections when F5 quickly

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -5,6 +5,11 @@ const socket = io(import.meta.env.VITE_SERVER_URL, {
   reconnection: true,
   reconnectionDelay: 1000,
   reconnectionAttempts: 5,
+  transports: ["websocket"],
+  upgrade: false,
+  auth: {
+    userId: getUserId(),
+  },
 });
 
 export default socket;
